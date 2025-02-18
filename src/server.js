c87121b565c9f6b908a9cae45bb769839f1428f3
Gustavo Santos <155779046+boyprogramming@users.noreply.github.com>
2025-02-18 11:56:09 +0000

    First Commit and Configurations

---
+++ src/server.js
@@ -0,0 +1,12 @@
+//const express = require('express');
+const express = require('express');
+const server = express();
+
+const PORT = process.env.PORT || 3000;
+
+server.get("/",(req,res)=>{ res.send("Hello World " + new Date());
+});
+
+server.listen(PORT,()=>{
+    console.log('server is running on port ${PORT}');
+});
